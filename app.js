298947a1d18fa4f46840c94ea8a5d2991d78e245
Mothmann <othman.lahlou00@gmail.com>
2022-06-06 20:31:44 +0100

    created empty project

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+var listener = app.listen(5000, function(){
+    console.log('Listening on port ' + listener.address().port);
+});
+
+module.exports = app;
