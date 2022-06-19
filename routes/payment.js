c72f1373bfeff98fe6dd7248e507101909e87e0f
Othman LAHLOU <56235490+Mothmann@users.noreply.github.com>
2022-06-19 19:54:51 +0100

    Update payment.js

--- routes/payment.js
+++ routes/payment.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const paymentController = require("../Controllers/payment");
 const cors = require("cors");
 
-router.post("/pay",cors(), paymentController.pay);
+router.post("/",cors(), paymentController.pay);
 
 module.exports = router;
